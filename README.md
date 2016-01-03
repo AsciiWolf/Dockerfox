c91609beb176829672d64ee75b2200c27311c5dd
AsciiWolf <mail@asciiwolf.com>
2016-01-03 02:32:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+### Dockerized Firefox with integrated Java and Flash.
+
+<br>
+
+**Installation:**
+
+Run: *./install.sh*
+
+
+**Usage:**
+
+The *dockerfox* command or *Mozilla Firefox (Dockerfox)* menu item.
---
+++ install.sh
@@ -0,0 +1,15 @@
+#/usr/bin/env bash
+
+echo -e "Building Docker image..."
+
+docker build -t dockerfox .
+
+echo -e "\nInstalling launcher..."
+
+mkdir -p ~/bin
+cp launcher/dockerfox ~/bin/
+cp launcher/dockerfox-java-settings ~/bin/
+
+mkdir -p ~/.local/share/applications
+cp launcher/dockerfox.desktop ~/.local/share/applications/
+cp launcher/dockerfox-java-settings.desktop ~/.local/share/applications/
